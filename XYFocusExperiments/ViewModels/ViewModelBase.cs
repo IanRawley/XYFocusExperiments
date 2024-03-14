545c7015355e365cfff64ff72305370559b026d6
Ian Rawley <ian.rawley@silmetra.co.nz>
2024-03-15 09:45:08 +1300

    Add project files.

---
+++ XYFocusExperiments/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace XYFocusExperiments.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ XYFocusExperiments/Views/MainView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace XYFocusExperiments.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+   
+}
